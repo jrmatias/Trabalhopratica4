3e39c50c200279af88a9c529c419661b2998679c
jrmatias <145656425+jrmatias@users.noreply.github.com>
2023-10-06 18:46:09 +0100

    Add files via upload

---
+++ Model/Student.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Student extends University member {
+
+    /**
+     * Default constructor
+     */
+    public Student() {
+    }
+
+    /**
+     * 
+     */
+    private int acctNo;
+
+    /**
+     * @return
+     */
+    public int getAcctNo() {
+        // TODO implement here
+        return 0;
+    }
+
+}
---
+++ Model/University member.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public abstract class University member {
+
+    /**
+     * Default constructor
+     */
+    public University member() {
+    }
+
+    /**
+     * 
+     */
+    public String firstName;
+
+    /**
+     * 
+     */
+    public void lastName; String;
+
+    /**
+     * 
+     */
+    public int ssNo;
+
+}
